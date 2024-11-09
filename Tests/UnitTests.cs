ec73687786396c683ad5480faa781c766cf3ec43
cong3054 <53188856+cong3054@users.noreply.github.com>
2024-11-08 21:59:09 -0600

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -54,11 +54,11 @@ namespace GithubActionsLab
             Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
             Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
         }
-        //Contains the test that will fail
+       
         [Test]
         public void Multiply_Valid()
         {
-            Assert.AreEqual(3, Program.Multiply("1", "2"));
+            Assert.AreEqual(2, Program.Multiply("1", "2"));
             Assert.AreEqual(6, Program.Multiply("3", "2"));
             Assert.AreEqual(35, Program.Multiply("5", "7"));
         }
